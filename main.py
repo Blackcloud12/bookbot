29f4d2745404c36ebd27dee525ae558a379d8116
Daren Davis <Blackcloud12@gmail.com>
2024-08-22 11:44:18 -0400

    Fixed sorting function and reversed print order

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
         print(f"---Begin report of {path} ---")
         print(f'{total} words found in the document')
         letter_dic = letter_count(file_content)
-        for k in letter_dic:
-            print(f"The '{k}' character was found {letter_dic[k]} times")
+        list_of_letters = sort_on(letter_dic)
+        # sort dic in decending order
+        for row in reversed(list_of_letters):
+            print(f"The '{row[1]}' character was found {row[0]} times")
         print("--- End report ---")
 
 def word_count(inputStr):
@@ -23,6 +25,11 @@ def letter_count(inputStr):
                 letter_dic[letter] = 1
     return letter_dic
 
-# sort dic in decending order
+def sort_on(letter_dict):
+    list_of_tuples = []
+    for pair in letter_dict.items():
+        list_of_tuples.append((pair[1],pair[0]))
+    list_of_tuples.sort()
+    return list_of_tuples
 
 main()
